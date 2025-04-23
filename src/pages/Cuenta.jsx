9dc2f1a09cc6448ac25a80941c646243fa8e4249
Gonzalo <h.munozampuero@gmail.com>
2025-04-23 19:16:07 -0400

    correcion style.ccs ruta

--- src/pages/Cuenta.jsx
+++ src/pages/Cuenta.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import ValorModal from "../components/ValorModal";
-import "../assets/styles/styles.css";
+import "../assets/styles.css";
 
 const Cuenta = () => {
   const [usuario, setUsuario] = useState(null);
