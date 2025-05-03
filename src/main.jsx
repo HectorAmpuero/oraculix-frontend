1e66e697684d542605eca0d2067203feeeaa8055
Gonzalo <h.munozampuero@gmail.com>
2025-05-03 12:44:30 -0400

    cambios total css 3

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./assets/index.css";
-import "./assets/Navbar.css";
 import "./assets/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
