69d5385825c02477c0dcd7e8a85f22ec0ee70d4a
Gonzalo <h.munozampuero@gmail.com>
2025-05-09 22:29:43 -0400

    cambios generales pixle

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./assets/styles.css";
+import MetaPixel from "./components/MetaPixel";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+  <MetaPixel />
   <BrowserRouter>
     <App />
   </BrowserRouter>
+</>
 );
