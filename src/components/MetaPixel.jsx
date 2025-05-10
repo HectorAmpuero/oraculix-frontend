604cb8fde08672ca5330f289556cdf5eba8d75a2
Gonzalo <h.munozampuero@gmail.com>
2025-05-09 22:42:19 -0400

    cambios metapixel 2

--- src/components/MetaPixel.jsx
+++ src/components/MetaPixel.jsx
@@ -2,41 +2,33 @@ import { useEffect } from "react";
 
 export default function MetaPixel() {
   useEffect(() => {
-    // Si ya está cargado, no lo cargues de nuevo
-    if (window.fbq) {
-      window.fbq("track", "PageView");
-      return;
+    const pixelId = "1059875085992000";
+
+    // Verifica que no esté cargado ya
+    if (!window.fbq) {
+      window.fbq = function () {
+        window.fbq.callMethod
+          ? window.fbq.callMethod.apply(window.fbq, arguments)
+          : window.fbq.queue.push(arguments);
+      };
+      window._fbq = window.fbq;
+      window.fbq.push = window.fbq;
+      window.fbq.loaded = true;
+      window.fbq.version = "2.0";
+      window.fbq.queue = [];
+
+      const script = document.createElement("script");
+      script.async = true;
+      script.src = "https://connect.facebook.net/en_US/fbevents.js";
+      document.head.appendChild(script);
     }
 
-    // Cargar el script del Pixel
-    !(function (f, b, e, v, n, t, s) {
-      if (f.fbq) return;
-      n = f.fbq = function () {
-        n.callMethod
-          ? n.callMethod.apply(n, arguments)
-          : n.queue.push(arguments);
-      };
-      if (!f._fbq) f._fbq = n;
-      n.push = n;
-      n.loaded = true;
-      n.version = "2.0";
-      n.queue = [];
-      t = b.createElement(e);
-      t.async = true;
-      t.src = v;
-      s = b.getElementsByTagName(e)[0];
-      s.parentNode.insertBefore(t, s);
-    })(window, document, "script", "https://connect.facebook.net/en_US/fbevents.js");
-
-    // Esperar un poco para asegurar que fbq esté listo
-    setTimeout(() => {
-      if (window.fbq) {
-        window.fbq("init", "1059875085992000");
-        window.fbq("track", "PageView");
-      }
-    }, 500); // medio segundo de espera
+    // Inicializa e intenta disparar PageView
+    window.fbq("init", pixelId);
+    window.fbq("track", "PageView");
   }, []);
 
   return null;
 }
+
 
